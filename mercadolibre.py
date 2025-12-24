3e55ba7bc781206c191d99932004c347e1ee448b
nicolasgallu <nicolasgallu@pm.me>
2025-12-24 14:14:27 -0300

    eng to spanish message

--- mercadolibre.py
+++ mercadolibre.py
@@ -22,6 +22,6 @@ def meli_metrics(credentials,meli_metrics_url):
     no_respondidas = data_row[3]
     share_respondidas = data_row[4]
     # 4. Separate variables
-    meli_metrics = (f"MercadoLibre Yesterday Metrics\n-preguntas: {preguntas}\n-respondidas: {respondidas}\n-no_respondidas: {no_respondidas}\n-share_respondidas: {share_respondidas}")
+    meli_metrics = (f"MercadoLibre Metricas de Ayer\n-preguntas: {preguntas}\n-respondidas: {respondidas}\n-no_respondidas: {no_respondidas}\n-share_respondidas: {share_respondidas}")
     return meli_metrics
     
